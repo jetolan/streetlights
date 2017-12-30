6670d8ade15eff22fb92ddfa22b5fae2a8647cfd
Jamie Tolan <jamie.tolan@gmail.com>
2017-12-30 12:51:51 -0800

    add necessary access token call

--- src/example.py
+++ src/example.py
@@ -1,6 +1,9 @@
 from mapbox import Geocoder
-
+import os
 geocoder=Geocoder()
+
+geocoder.session.params['access_token'] == os.environ['MAPBOX_ACCESS_TOKEN']
+
 
 response = geocoder.forward("200 queen street")
 print(response.status_code)
